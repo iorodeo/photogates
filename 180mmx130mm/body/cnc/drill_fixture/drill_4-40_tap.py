677c323e6e60cd7d808f05e881b89ddbf940d48f
iorodeo <iorodeo@localhost>
2014-08-03 16:23:42 -0700

    Added drilling fixture for drilling sensor mount holes.

---
+++ 180mmx130mm/body/cnc/drill_fixture/drill_4-40_tap.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+import os 
+import sys
+from py2gcode import gcode_cmd
+from py2gcode import cnc_dxf
+
+feedrate = 15.0
+
+prog = gcode_cmd.GCodeProg()
+prog.add(gcode_cmd.GenericStart())
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.FeedRate(feedrate))
+
+param = { 
+        'fileName'    : 'drill_fixture.dxf',
+        'layers'      : ['drill_4-40_tap'],
+        'dxfTypes'    : ['CIRCLE'],
+        'startZ'      : 0.00,
+        'stopZ'       : -0.5,
+        'safeZ'       : 1.0,
+        'stepZ'       : 0.01,
+        'startDwell'  : 2.0,
+        }
+drill = cnc_dxf.DxfDrill(param)
+prog.add(drill)
+
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.End(),comment=True)
+baseName, dummy = os.path.splitext(__file__)
+fileName = '{0}.ngc'.format(baseName)
+print('generating: {0}'.format(fileName))
+prog.write(fileName)

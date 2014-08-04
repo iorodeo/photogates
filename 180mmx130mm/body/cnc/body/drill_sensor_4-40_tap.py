648ef45ee22c1dd5782cfe74df66f76e85cc35b2
iorodeo <iorodeo@localhost>
2014-08-03 20:26:42 -0700

    Added drilling fixture and sensor mount hole drilling.

---
+++ 180mmx130mm/body/cnc/body/drill_sensor_4-40_tap.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+import os 
+import sys
+from py2gcode import gcode_cmd
+from py2gcode import cnc_dxf
+
+feedrate = 30.0
+
+prog = gcode_cmd.GCodeProg()
+prog.add(gcode_cmd.GenericStart())
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.FeedRate(feedrate))
+
+param = { 
+        'fileName'    : 'sensor_mount_holes.dxf',
+        'dxfTypes'    : ['CIRCLE'],
+        'startZ'      : 0.0,
+        'stopZ'       : -0.4,
+        'safeZ'       : 0.5,
+        'stepZ'       : 0.02,
+        'startDwell'  : 2.0,
+        }
+drill = cnc_dxf.DxfDrill(param)
+prog.add(drill)
+
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.End(),comment=True)
+baseName, dummy = os.path.splitext(__file__)
+fileName = '{0}.ngc'.format(baseName)
+print('generating: {0}'.format(fileName))
+prog.write(fileName)
---
+++ 180mmx130mm/body/cnc/drill_fixture/drill_0p25-20_thru_router.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+import os 
+import sys
+from py2gcode import gcode_cmd
+from py2gcode import cnc_dxf
+
+feedrate = 15.0
+
+prog = gcode_cmd.GCodeProg()
+prog.add(gcode_cmd.GenericStart())
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.FeedRate(feedrate))
+
+param = { 
+        'fileName'    : 'drill_fixture.dxf',
+        'layers'      : ['drill_0p25-20_thru'],
+        'dxfTypes'    : ['CIRCLE'],
+        'startZ'      : 0.00,
+        'stopZ'       : -1.70,
+        'safeZ'       : 1.0,
+        'stepZ'       : 0.01,
+        'startDwell'  : 2.0,
+        }
+drill = cnc_dxf.DxfDrill(param)
+prog.add(drill)
+
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.End(),comment=True)
+baseName, dummy = os.path.splitext(__file__)
+fileName = '{0}.ngc'.format(baseName)
+print('generating: {0}'.format(fileName))
+prog.write(fileName)
